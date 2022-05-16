cf05f3ce681fb9c6aff8eaebf68dccd11f01e886
Jonathan Starr <jonstarr67@gmail.com.com>
2022-05-16 10:53:02 -0500

    Switch to questionary for user prompting

--- shippy/console.py
+++ shippy/console.py
@@ -3,35 +3,23 @@
 import typing
 import difflib
 
-import PyInquirer
+import questionary
 
 
 def query_unit(units: typing.Dict[str, int]) -> typing.Optional[str]:
     """Query a name of a unit from the user."""
-    answer = PyInquirer.prompt(
-        {"type": "input", "name": "unit", "message": "Enter name of unit:"}
-    )
-    unit = answer.get("unit")
+    unit = questionary.text("Enter name of unit:").ask().upper()
     if unit is None:
         return None
 
+    unit = unit.upper()
+
     def get_matches(unit):
         num_matches, cutoff = 4, 0.0
-        return difflib.get_close_matches(unit, units, num_matches, cutoff)
+        return difflib.get_close_matches(unit, list(units), num_matches, cutoff)
 
     matches = get_matches(unit)
-    choices = [dict(name=match) for match in matches]
-
-    answer = PyInquirer.prompt(
-        {
-            "type": "list",
-            "message": "Select match:",
-            "name": "unit",
-            "choices": choices,
-        }
-    )
-    unit = answer.get("unit")
-    return unit
+    return questionary.select("Select match:", choices=matches).ask()
 
 
 def query_weight() -> typing.Optional[int]:
@@ -41,22 +29,14 @@ def query_weight() -> typing.Optional[int]:
         try:
             weight = int(weight)
         except (TypeError, ValueError):
-            return "Weight must be an integer value."
+            return "Weight must be an integer."
 
         if weight <= 0:
-            return "Weight must be positive."
+            return "Weight must be strictly positive."
 
         return True
 
-    answer = PyInquirer.prompt(
-        {
-            "type": "input",
-            "name": "weight",
-            "message": "Please enter weight in pounds:",
-            "validate": validate,
-        }
-    )
-    weight = answer.get("weight")
+    weight = questionary.text("Please enter weight in pounds:", validate=validate).ask()
     return weight and int(weight)
 
 
@@ -72,26 +52,20 @@ def query_request_id() -> typing.Optional[
             try:
                 int(request_id)
             except ValueError:
-                return "Invalid request ID."
+                return "Request ID must be an integer."
             else:
                 return True
         else:
             try:
                 int(inmate_id), int(index)
             except ValueError:
-                return "Invalid request ID."
+                return "Inmate ID and index must be an integer."
             else:
                 return True
 
-    answer = PyInquirer.prompt(
-        {
-            "type": "input",
-            "name": "request_id",
-            "message": "Please scan or enter the request ID:",
-            "validate": validate,
-        }
-    )
-    request_id = answer["request_id"]
+    request_id = questionary.text(
+        "Please enter the request ID:", validate=validate
+    ).ask()
 
     if request_id is None:
         return None
@@ -106,14 +80,22 @@ def query_request_id() -> typing.Optional[
 
 def query_address() -> typing.Optional[typing.Dict[str, str]]:
     """Query an address from the user."""
-    answers = PyInquirer.prompt(
-        [
-            {"type": "input", "name": "name", "message": "Enter name:"},
-            {"type": "input", "name": "street1", "message": "Enter street1:"},
-            {"type": "input", "name": "street2", "message": "Enter street2:"},
-            {"type": "input", "name": "city", "message": "Enter city:"},
-            {"type": "input", "name": "state", "message": "Enter state:"},
-            {"type": "input", "name": "zipcode", "message": "Enter zipcode:"},
-        ]
-    )
-    return answers or None
+    prompts = {
+        "name": "Enter name:",
+        "street1": "Enter street1:",
+        "street2": "Enter street2:",
+        "city": "Enter city:",
+        "state": "Enter state:",
+        "zipcode": "Enter zipcode:",
+    }
+
+    questions = {name: questionary.text(prompt) for name, prompt in prompts.items()}
+
+    address = {}
+    for name, question in questions.items():
+        response = question.ask()
+        if response is None:
+            return None
+        address[name] = response
+
+    return address
