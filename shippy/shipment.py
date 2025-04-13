4f076b22493d2731a03c48bb76c79ab6597a9b73
Jonathan Starr <github@jstarr.me>
2025-04-13 14:07:52 -0500

    shipping: update for `easypost` API changes

--- shippy/shipment.py
+++ shippy/shipment.py
@@ -2,48 +2,44 @@
 
 import easypost
 
+from easypost.models import Address as EasyPostAddress
+from easypost.models import Parcel as EasyPostParcel
+from easypost.models import Shipment as EasyPostShipment
+
 
 # pylint: disable=too-few-public-methods
 class Builder:
     """Shipment builder."""
 
-    def __init__(self, apikey):
+    def __init__(self, apikey: str):
         """Initialize shipment builder with an apikey."""
-        self._apikey = apikey
+        self._client = easypost.EasyPostClient(apikey)
 
-    def _address_from_dict(self, dict_):
-        """Convert a dict to an address."""
-        dict_ = dict(dict_)
-        dict_["zip"] = dict_.pop("zipcode")
-        dict_["country"] = "US"
-        dict_["phone"] = ""
-        return easypost.Address.create_and_verify(api_key=self._apikey, **dict_)
+    def _build_address(self, **kwargs) -> EasyPostAddress:
+        """Build easypost Address."""
+        kwargs["zip"] = kwargs.pop("zipcode")
+        kwargs["country"] = "US"
+        kwargs["phone"] = ""
+        return self._client.address.create_and_verify(**kwargs)
 
-    def _build_parcel(self, weight):
-        """Build easypost Parcel given weight."""
-        return easypost.Parcel.create(
-            api_key=self._apikey, predefined_package="Parcel", weight=weight
+    def _build_parcel(self, weight: int) -> EasyPostParcel:
+        """Build easypost Parcel given weight in ounces."""
+        return self._client.parcel.create(predefined_package="Parcel", weight=weight)
+
+    def _build_shipment(self, from_address: dict, to_address: dict, weight: int) -> EasyPostShipment:
+        """Build easypost Shipment given addresses and weight in ounces."""
+        return self._client.shipment.create(
+            from_address=self._build_address(**from_address),
+            to_address=self._build_address(**to_address),
+            parcel=self._build_parcel(weight),
+            options=dict(special_rates_eligibility="USPS.LIBRARYMAIL"),
         )
 
-    def _build_shipment(self, from_addr, to_addr, weight):
-        """Build shipment from easypost addresses and weight."""
-        parcel = self._build_parcel(weight)
-        options = dict(special_rates_eligibility="USPS.LIBRARYMAIL")
-        return easypost.Shipment.create(
-            api_key=self._apikey,
-            from_address=from_addr,
-            to_address=to_addr,
-            parcel=parcel,
-            options=options,
-        )
-
-    def __call__(self, from_addr, to_addr, weight):
-        """Purchase postage of a Parcel with a given weight in ounces."""
-        from_addr, to_addr = map(self._address_from_dict, (from_addr, to_addr))
-        shipment = self._build_shipment(from_addr, to_addr, weight)
+    def __call__(self, from_address: dict, to_address: dict, weight: int) -> EasyPostShipment:
+        """Purchase postage given addresses and weight in ounces."""
+        shipment = self._build_shipment(from_address, to_address, weight)
         rate = shipment.lowest_rate(["USPS"])
-        shipment.buy(rate=rate)
-        return shipment
+        return self._client.shipment.buy(shipment.id, rate=rate)
 
 
 def extract_data(shipment):
